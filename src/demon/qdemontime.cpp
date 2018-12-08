540e915af772e06777a2911cc19b8fdd35442bf2
Andy Nichols <nezticle@gmail.com>
2018-12-08 13:59:26 +0100

    Add <cronos> timer.  builds but untested

--- src/demon/qdemontime.cpp
+++ src/demon/qdemontime.cpp
@@ -1,60 +1,62 @@
 #include <QtDemon/qdemontime.h>
 
+#include <chrono>
+
 QT_BEGIN_NAMESPACE
 
-//namespace {
-//::qt3ds::QT3DSI64 getTimeTicks()
-//{
-//    LARGE_INTEGER a;
-//    QueryPerformanceCounter(&a);
-//    return a.QuadPart;
-//}
+namespace {
+quint64 getTimeTicks()
+{
+    quint64 a;
+    a = std::chrono::high_resolution_clock::rep();
+    return a;
+}
 
-//double getTickDuration()
-//{
-//    LARGE_INTEGER a;
-//    QueryPerformanceFrequency(&a);
-//    return 1.0f / double(a.QuadPart);
-//}
+double getTickDuration()
+{
+    quint64 a;
+    a = std::chrono::high_resolution_clock::period::den;
+    return 1.0 / double(a);
+}
 
-//double sTickDuration = getTickDuration();
-//} // namespace
+double sTickDuration = getTickDuration();
+} // namespace
 
-//const QDemonTimeCounterFrequencyToTensOfNanos QDemonTime::sCounterFreq = QDemonTime::getCounterFrequency();
+const QDemonTimeCounterFrequencyToTensOfNanos QDemonTime::sCounterFreq = QDemonTime::getCounterFrequency();
 
-//QDemonTimeCounterFrequencyToTensOfNanos QDemonTime::getCounterFrequency()
-//{
-//    LARGE_INTEGER freq;
-//    QueryPerformanceFrequency(&freq);
-//    return CounterFrequencyToTensOfNanos(Time::sNumTensOfNanoSecondsInASecond, freq.QuadPart);
-//}
+QDemonTimeCounterFrequencyToTensOfNanos QDemonTime::getCounterFrequency()
+{
+    quint64 freq;
+    freq = std::chrono::high_resolution_clock::period::den;
+    return QDemonTimeCounterFrequencyToTensOfNanos(QDemonTime::sNumTensOfNanoSecondsInASecond, freq);
+}
 
-//quint64 QDemonTime::getCurrentCounterValue()
-//{
-//    LARGE_INTEGER ticks;
-//    QueryPerformanceCounter(&ticks);
-//    return ticks.QuadPart;
-//}
+quint64 QDemonTime::getCurrentCounterValue()
+{
+    quint64 ticks;
+    ticks = std::chrono::high_resolution_clock::rep();
+    return ticks;
+}
 
-//QDemonTime::QDemonTime()
-//    : mTickCount(0)
-//{
-//    getElapsedSeconds();
-//}
+QDemonTime::QDemonTime()
+    : mTickCount(0)
+{
+    getElapsedSeconds();
+}
 
-//QDemonTime::Second QDemonTime::getElapsedSeconds()
-//{
-//    QT3DSI64 lastTickCount = mTickCount;
-//    mTickCount = getTimeTicks();
-//    return (mTickCount - lastTickCount) * sTickDuration;
-//}
+QDemonTime::Second QDemonTime::getElapsedSeconds()
+{
+    qint64 lastTickCount = mTickCount;
+    mTickCount = getTimeTicks();
+    return (mTickCount - lastTickCount) * sTickDuration;
+}
 
-//QDemonTime::Second QDemonTime::peekElapsedSeconds()
-//{
-//    return (getTimeTicks() - mTickCount) * sTickDuration;
-//}
+QDemonTime::Second QDemonTime::peekElapsedSeconds()
+{
+    return (getTimeTicks() - mTickCount) * sTickDuration;
+}
 
-//QDemonTime::Second QDemonTime::getLastTime() const { return mTickCount * sTickDuration; }
+QDemonTime::Second QDemonTime::getLastTime() const { return mTickCount * sTickDuration; }
 
 
 QT_END_NAMESPACE
