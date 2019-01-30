4ec7d01da2f6f0d5d65873e107a014a4c6af5ea7
Christian Strømme <christian.stromme@qt.io>
2019-01-30 15:56:05 +0100

    Use std::abs to avoid float to double conversion

--- src/runtimerender/rendererimpl/qdemonrendererimpl.h
+++ src/runtimerender/rendererimpl/qdemonrendererimpl.h
@@ -81,10 +81,10 @@ inline bool ISRenderObjectPtrGreatThan(const SRenderableObject *lhs,
 {
     return FloatLessThan(rhs->m_CameraDistanceSq, lhs->m_CameraDistanceSq);
 }
-inline bool NonZero(float inValue) { return fabs(inValue) > .001f; }
+inline bool NonZero(float inValue) { return std::abs(inValue) > .001f; }
 inline bool NonZero(quint32 inValue) { return inValue != 0; }
-inline bool IsZero(float inValue) { return fabs(inValue) < .001f; }
-inline bool IsNotOne(float inValue) { return fabs(1.0f - inValue) > .001f; }
+inline bool IsZero(float inValue) { return std::abs(inValue) < .001f; }
+inline bool IsNotOne(float inValue) { return std::abs(1.0f - inValue) > .001f; }
 
 inline bool IsRectEdgeInBounds(qint32 inNewRectOffset, qint32 inNewRectWidth,
                                qint32 inCurrentRectOffset, qint32 inCurrentRectWidth)
