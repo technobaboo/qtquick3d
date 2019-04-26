894555eecc115e007b6a99bcc7a053f2f117199e
Christian Strømme <christian.stromme@qt.io>
2019-04-26 15:13:31 +0200

    Make RenderDoc happy on linux
    We're using context sharing atm, and since the version for QtQuick,
    which is what RenderDoc will see first, is lower then the required
    3.2+, it will refuse to capture anything.

--- examples/quick3d/simple/main.cpp
+++ examples/quick3d/simple/main.cpp
@@ -1,11 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QtGui>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+#ifdef Q_OS_LINUX
+    // We need this for renderdoc
+    QSurfaceFormat df = QSurfaceFormat::defaultFormat();
+    df.setVersion(3, 3);
+    QSurfaceFormat::setDefaultFormat(df);
+#endif
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
